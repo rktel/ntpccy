f05647eaabf070806823c5cdb20f7469796e2e66
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2018-11-29 10:43:27 -0500

    login change n6

--- imports/api/routes.js
+++ imports/api/routes.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
-
+// Layouts
 import Login from '../ui/layouts/Login.vue'
+import Home from '../ui/layouts/Home.vue'
 
 const routes = [
-    { name: 'Login', path: '/login', component: Login }
+    { name: 'Login', path: '/login', component: Login },
+    { name: 'Home', path: '/', component: Home, redirect:'/home' },
 ]
 
 export default new VueRouter({
---
+++ imports/ui/layouts/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <section>
+        <h1>On Home</h1>
+    </section>
+</template>
+
+<script>
+export default {
+name: 'Home'
+}
+</script>
+
+<style>
+
+</style>
