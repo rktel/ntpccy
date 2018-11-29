37145adfc09fd8f8d329a10915e26ed4b3c23897
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2018-11-29 12:21:59 -0500

    login change n12

--- imports/ui/layouts/Home.vue
+++ imports/ui/layouts/Home.vue
@@ -1,16 +1,25 @@
 <template>
   <v-app>
     <v-content>
-      <v-container fluid fill-height>
-          <h1>Home Page</h1>
+      <v-container>
+        <h1>Home Page</h1>
+        <ul>
+            <li v-for="name in names" :key="name" v-text="name"></li>
+        </ul>
       </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
+import { mapState, mapGetters, mapMutations } from "vuex";
 export default {
-  name: "Home"
+  name: "Home",
+  computed:{
+      ...mapState([
+          'names'
+      ]),
+  }
 };
 </script>
 
