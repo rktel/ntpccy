8e113a0a95805cdf3f42e6b0d6a50c1bee23b4ed
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2018-11-29 12:01:02 -0500

    login change n9

--- imports/api/routes.js
+++ imports/api/routes.js
@@ -7,8 +7,9 @@ import Login from '../ui/layouts/Login.vue'
 import Home from '../ui/layouts/Home.vue'
 
 const routes = [
+    { path: '/', redirect:'/home'},
     { name: 'Login', path: '/login', component: Login },
-    { name: 'Home', path: '/', component: Home},
+    { name: 'Home', path: '/home', component: Home},
 ]
 
 export default new VueRouter({
