453701626c006aa08e3ce537e5a1c9a026a87b4b
rktel <rakinakuy@hotmail.com>
2018-12-18 16:41:17 -0500

    add apiItems HomeUsers 31

--- server/main.js
+++ server/main.js
@@ -3,8 +3,11 @@ import { Personal } from '../imports/api/collections'
 let hyperUserId = null
  
 Meteor.startup(() => {
-    console.log('Meteor backend up')
-    console.log('process.env.MAIL_URL:',process.env.MAIL_URL)
+    process.env.MAIL_URL = 'smtp://' + encodeURIComponent(Meteor.settings.private.USER_SMTP) + 
+    ':' + encodeURIComponent(Meteor.settings.private.PASS_SMTP) + 
+    '@' + encodeURIComponent(Meteor.settings.private.SERVER_SMTP) + ':' + Meteor.settings.private.PORT_SMTP;
+
+    console.log('process.env.MAIL_URL', process.env.MAIL_URL)
 
     if (Meteor.users.find().count() == 0) {
         hyperUserId = Accounts.createUser({
