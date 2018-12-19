6a1344a358a0ac7b2a557d39137bde7bc0f98b08
rktel <rakinakuy@hotmail.com>
2018-12-19 17:28:23 -0500

    add img for enterprise 8

--- imports/ui/layouts/Home.vue
+++ imports/ui/layouts/Home.vue
@@ -41,7 +41,7 @@
 
     <v-content>
       <section :style="{'position':'absolute', 'left':'20px', 'top':'10px'}">
-        <v-img src="/img/Antapaccay.png" height="20">
+        <v-img src="/img/Antapaccay.png" height="20"/>
       </section>
       <section :style="{'position':'absolute', 'right':'20px', 'top':'10px'}">
         <p>Bienvenid@ {{userProfile.firstname}}</p>
