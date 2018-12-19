637c549b49ad77d481558a7ab7fe0bff95d817e8
rktel <rakinakuy@hotmail.com>
2018-12-19 17:20:54 -0500

    add img for enterprise 4

--- imports/ui/layouts/Home.vue
+++ imports/ui/layouts/Home.vue
@@ -41,7 +41,8 @@
 
     <v-content>
       <section :style="{'position':'absolute', 'left':'20px', 'top':'10px'}">
-        <h3>{{userProfile.api}}</h3>
+        
+        <v-img v-if="userProfile.api" :src="`/img/${userProfile.api}.png`" height="20"></v-img>
       </section>
       <section :style="{'position':'absolute', 'right':'20px', 'top':'10px'}">
         <p>Bienvenid@ {{userProfile.firstname}}</p>
