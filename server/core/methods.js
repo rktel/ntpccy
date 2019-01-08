1c7f8b9d788e402adb9fa0a5d46461fb376d5101
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-01-08 14:16:26 -0500

    Servosa again 36

--- server/core/methods.js
+++ server/core/methods.js
@@ -94,8 +94,8 @@ Meteor.methods({
         const report = await Servosa.rawCollection().
             aggregate([
                 { $match: { 'events.vehicle': plate, 'events.created': { $gte: dateTimeStart, $lte: dateTimeEnd } } },
-                { $group: { _id: { plate: '$events.vehicle', created: '$events.created' } } },
-                { $project: { plate: '$_id.plate' } }
+                { $group: { _id: { plate: '$events.vehicle', created: '$events.created', event: '$events.original' } } },
+                { $project: { _id: 0, plate: 1, event: 1 } }
 
             ]).toArray()
         return report
