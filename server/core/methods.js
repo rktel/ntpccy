32488a60dca738913e4064f016f9f72169b6c99d
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-01-10 10:44:46 -0500

    Servosa get Data 9

--- server/core/methods.js
+++ server/core/methods.js
@@ -93,10 +93,10 @@ Meteor.methods({
     async  Servosa_getData(el, dateTimeStart, dateTimeEnd) {
         const report = await Servosa.rawCollection().
             aggregate([
-                { $match: { 'events.vehicle': el, 'events.created': { $gte: dateTimeStart, $lte: dateTimeEnd }, 'events.original': { $in: [81, 82] } } },
+                { $match: { 'events.vehicle': el, 'events.created': { $gte: dateTimeStart, $lte: dateTimeEnd }, 'events.original': { $in: [ 82] } } },
                 { $unwind: '$events' },
-                { $group: { _id: { plate: '$events.vehicle', ev: '$events.original' }, total: { $sum: 1 } } },
-                { $project: { _id: 0, plate: '$_id.plate', event: '$_id.ev' , total: '$total'} },
+               // { $group: { _id: { plate: '$events.vehicle', ev: '$events.original' }, total: { $sum: 1 } } },
+               // { $project: { _id: 0, plate: '$_id.plate', event: '$_id.ev' , total: '$total'} },
                 // { $group: { _id: { plate: '$events.vehicle', ev: '$events.original' }, total: { $sum: 1 } } },
                 // { $group: { _id: { plate: '$events.vehicle', created: '$events.created', event: '$events.original' }} },
                 //   { $project: { _id: 0, plate: '$_id.plate', event: '$_id.event', created: '$_id.created' } },
