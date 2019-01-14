890d484986e1c8986e958a7bad9b4f3c4a1dffb1
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-01-14 16:32:29 -0500

    ExsaKm add kmValue 17

--- server/core/methods.js
+++ server/core/methods.js
@@ -94,14 +94,11 @@ Meteor.methods({
                 { $match: { 'events.vehicle': { $in: plates }, 'events.created': { $gte: dateTimeStart, $lte: dateTimeEnd } } },
                 { $unwind: '$events' },
                 { $match: { 'events.location.speed': { $gte: kmValue } } },
-                { $count: 'total' }
-                /*
+                { $count: 'total' },
                 { $group: { _id: { plate: '$events.vehicle', kmValue: '$events.location.speed' }, total: { $sum: 1 } } },
-                
                 { $project: { _id: 0, plate: '$_id.plate', kmValue: '$_id.kmValue', total: '$total' } },
-
                 { $sort: { 'plate': 1 } }
-                */
+
             ]).toArray()
         return report
 
