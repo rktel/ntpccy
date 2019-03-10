89b4fbe825a85989f702946f49502155331b8d17
rktel <rakinakuy@hotmail.com>
2019-03-10 02:37:46 -0500

    add Dinet get_data test 24

--- server/core/methods.js
+++ server/core/methods.js
@@ -462,15 +462,16 @@ console.log("=====================================");
                             
                             
                             if (report[index].plate == report[index + 1].plate) {
-                                RowArray.push({placa: report[index].plate})
                                 // La placa actual es igual a la placa siguiente
+                                RowArray.push({placa: report[index].plate})
                                 if (report[index].eventType == type0 && !RowArray[index].type0) RowArray[index].type0 = report[index].total
                                 if (report[index].eventType == type1 && !RowArray[index].type1) RowArray[index].type1 = report[index].total
-
+                                
                                 if (report[index + 1].eventType == type0 && !RowArray[index].type0) RowArray[index].type0 = report[index + 1].total
                                 if (report[index + 1].eventType == type1 && !RowArray[index].type1) RowArray[index].type1 = report[index + 1].total
                             } else {
                                 // La placa actual es diferente a la placa siguiente
+                                RowArray.push({placa: report[index].plate})
                                 if (report[index].eventType == type0 && !RowArray[index].type0) RowArray[index].type0 = report[index].total
                                 if (report[index].eventType == type1 && !RowArray[index].type1) RowArray[index].type1 = report[index].total
                             }
