a1f2d9c948fdfb9315ef694954fc915dcc8f87e6
rktel <rakinakuy@hotmail.com>
2019-04-24 16:31:00 -0500

    check Servosa 9

--- server/core/methods.js
+++ server/core/methods.js
@@ -419,7 +419,7 @@ Meteor.methods({
 
         Meteor.call('Servosa_getData', plates, dateTimeStart5, dateTimeEnd5, (error, report) => {
             console.log('Report:', report)
-            
+
             /*
 
             if (!error) {
@@ -514,8 +514,8 @@ Meteor.methods({
          *  305     81          Fatiga
          *  0       80          No Rostro
          */
-         const eventType_Array = [305, 306,0]
-         const eventOriginal_Array = [82, 81, 80]
+        const eventType_Array = [305, 306, 0]
+        const eventOriginal_Array = [82, 81, 80]
         // const arrayEvents = [81,82]
 
         const report = await Servosa.rawCollection().
@@ -525,72 +525,80 @@ Meteor.methods({
                 { $unwind: '$events' },
                 { $match: { 'events.original': { $in: eventOriginal_Array } } },
                 {
-                    $group:{
-                        _id:'$events.vehicle',
+                    $group: {
+                        _id: '$events.vehicle',
                         fatigaYDistraccion: {
                             $sum: {
-                                $cond:{
-                                    if:{
-                                        $or:[
-                                            {$and:[
-                                                { $eq: ['$events.original', 82] },
-                                                { $eq: ['$events.type', 306] },
-                                            ]},
-                                            {$and:[
-                                                { $eq: ['$events.original', 81] },
-                                                { $eq: ['$events.type', 305] },
-                                            ]}
+                                $cond: {
+                                    if: {
+                                        $or: [
+                                            {
+                                                $and: [
+                                                    { $eq: ['$events.original', 82] },
+                                                    { $eq: ['$events.type', 306] },
+                                                ]
+                                            },
+                                            {
+                                                $and: [
+                                                    { $eq: ['$events.original', 81] },
+                                                    { $eq: ['$events.type', 305] },
+                                                ]
+                                            }
                                         ]
                                     },
-                                    then:1,
-                                    else:0
+                                    then: 1,
+                                    else: 0
                                 }
                             }
                         },
-                        noRostro:{
-                            $sum:{
-                                $cond:{
-                                    if:{
-                                        $and:[
-                                            {$eq: ['$events.original', 80]},
-                                            {$eq: ['$events.type', 0]}
+                        noRostro: {
+                            $sum: {
+                                $cond: {
+                                    if: {
+                                        $and: [
+                                            { $eq: ['$events.original', 80] },
+                                            { $eq: ['$events.type', 0] }
                                         ]
                                     },
-                                    then:1,
-                                    else:0
+                                    then: 1,
+                                    else: 0
                                 }
                             }
                         }
-                    }
-                }
- 
+                    },
+                  
+                  
+                },
+                { $project: { _id: 0, placa: '$_id', fatigaYDistraccion: '$fatigaYDistraccion', noRostro: '$noRostro' } },
+                { $sort: { 'placa': 1 } },
+
             ]).toArray()
 
-            console.log(report);
-            
+        console.log(report);
+
         return report
 
     }
-/*
-    async  Servosa_getData(plates, dateTimeStart, dateTimeEnd) {
-        const arrayEvents = [305, 306]
-
-        const report = await Servosa.rawCollection().
-            aggregate([
-                // { $match: { 'events.vehicle': el, 'events.created': { $gte: dateTimeStart, $lte: dateTimeEnd }, 'events.original': { $in: [ 81,82] } } },
-                { $match: { 'events.vehicle': { $in: plates }, 'events.created': { $gte: dateTimeStart, $lte: dateTimeEnd } } },
-                { $unwind: '$events' },
-                { $match: { 'events.type': { $in: arrayEvents } } },
-                { $group: { _id: { plate: '$events.vehicle', eventType: '$events.type' }, total: { $sum: 1 } } },
-                { $project: { _id: 0, plate: '$_id.plate', eventType: '$_id.eventType', total: '$total' } },
-                // { $group: { _id: { plate: '$events.vehicle', created: '$events.created', event: '$events.original' }} },
-                //   { $project: { _id: 0, plate: '$_id.plate', event: '$_id.event', created: '$_id.created' } },
-                { $sort: { 'plate': 1, 'eventType': 1 } },
-            ]).toArray()
-        return report
-
-    }
-*/
+    /*
+        async  Servosa_getData(plates, dateTimeStart, dateTimeEnd) {
+            const arrayEvents = [305, 306]
+    
+            const report = await Servosa.rawCollection().
+                aggregate([
+                    // { $match: { 'events.vehicle': el, 'events.created': { $gte: dateTimeStart, $lte: dateTimeEnd }, 'events.original': { $in: [ 81,82] } } },
+                    { $match: { 'events.vehicle': { $in: plates }, 'events.created': { $gte: dateTimeStart, $lte: dateTimeEnd } } },
+                    { $unwind: '$events' },
+                    { $match: { 'events.type': { $in: arrayEvents } } },
+                    { $group: { _id: { plate: '$events.vehicle', eventType: '$events.type' }, total: { $sum: 1 } } },
+                    { $project: { _id: 0, plate: '$_id.plate', eventType: '$_id.eventType', total: '$total' } },
+                    // { $group: { _id: { plate: '$events.vehicle', created: '$events.created', event: '$events.original' }} },
+                    //   { $project: { _id: 0, plate: '$_id.plate', event: '$_id.event', created: '$_id.created' } },
+                    { $sort: { 'plate': 1, 'eventType': 1 } },
+                ]).toArray()
+            return report
+    
+        }
+    */
 
 });
 //-------------------- EXSA
