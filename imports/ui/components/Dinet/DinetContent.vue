9235f099d2eca42ab301be083ee2daa0422ef01b
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-04-30 15:02:09 -0500

    Dinet SPA 26

---
+++ imports/ui/components/Dinet/DinetContent.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-content>
+        <v-container fill-height>
+            <v-btn class="text-none">Start</v-btn>
+        </v-container>
+    </v-content>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- imports/ui/layouts/Dinet.vue
+++ imports/ui/layouts/Dinet.vue
@@ -1,14 +1,16 @@
 <template>
     <v-app>
           <DinetToolbar></DinetToolbar>
+          <DinetContent></DinetContent>
     </v-app>
 </template>
 
 <script>
 import DinetToolbar from "../../ui/components/Dinet/DinetToolbar.vue";
+import DinetContent from "../../ui/components/Dinet/DinetContent.vue";
 export default {
   components: {
-    DinetToolbar
+    DinetToolbar,DinetContent
   }
 };
 </script>
