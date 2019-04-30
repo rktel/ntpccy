4881becc67a64eb239f93f2a22a6c90d8a0b60f2
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-04-30 16:02:41 -0500

    Dinet SPA 33

--- imports/ui/components/Dinet/DinetToolbar.vue
+++ imports/ui/components/Dinet/DinetToolbar.vue
@@ -4,19 +4,21 @@
       <v-avatar color="grey lighten-4">
         <img src="img/Dinet_alt.png" alt="avatar">
       </v-avatar>
-      <v-btn icon @click="invertColor">
-        <v-icon>invert_colors</v-icon>
-      </v-btn>
-      <v-menu offset-x offset-y>
-        <v-btn icon slot="activator">
-          <v-icon>account_circle</v-icon>
+      <v-flex>
+        <v-btn icon @click="invertColor">
+          <v-icon>invert_colors</v-icon>
         </v-btn>
-        <v-list dense>
-          <v-list-tile v-for="(item,key) in profileMenu" :key="key" @click="item.action">
-            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-          </v-list-tile>
-        </v-list>
-      </v-menu>
+        <v-menu offset-x offset-y>
+          <v-btn icon slot="activator">
+            <v-icon>account_circle</v-icon>
+          </v-btn>
+          <v-list dense>
+            <v-list-tile v-for="(item,key) in profileMenu" :key="key" @click="item.action">
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile>
+          </v-list>
+        </v-menu>
+      </v-flex>
     </v-layout>
   </v-toolbar>
 </template>
@@ -39,7 +41,7 @@ export default {
       });
     },
     invertColor() {
-      Session.set("dark", !Session.get("dark"))
+      Session.set("dark", !Session.get("dark"));
     }
   }
 };
