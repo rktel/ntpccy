597491848a3ac91e4d32d03dd441fdcfdd160222
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-04-30 17:35:41 -0500

    Dinet SPA 41

--- imports/ui/components/Dinet/DinetContentToolbar.vue
+++ imports/ui/components/Dinet/DinetContentToolbar.vue
@@ -1,15 +1,46 @@
 <template>
-    <v-toolbar color="transparent" flat>
-        <v-toolbar-title class="px-4"> <span class="title font-italic grey--text text--lighten-1">Control de Velocidad</span></v-toolbar-title>
-    </v-toolbar>
+  <v-toolbar color="transparent" flat>
+    <v-toolbar-title class="px-4">
+      <span class="title font-italic grey--text">Control de Velocidad</span>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-dialog
+      ref="dialog"
+      persistent
+      v-model="modal"
+      lazy
+      full-width
+      width="290px"
+      :return-value.sync="date"
+    >
+      <v-text-field
+        slot="activator"
+        label="Fecha de Actualizacion"
+        v-model="date"
+        append-icon="event"
+        placeholder=" "
+        readonly
+        outline
+      ></v-text-field>
+      <v-date-picker v-model="date" scrollable>
+        <v-spacer></v-spacer>
+        <v-btn flat color="primary" @click="modal = false">Cancel</v-btn>
+        <v-btn flat color="primary" @click="$refs.dialog.save(date)">OK</v-btn>
+      </v-date-picker>
+    </v-dialog>
+  </v-toolbar>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      date: null,
+      modal: false
+    };
+  }
+};
 </script>
 
 <style>
-
 </style>
