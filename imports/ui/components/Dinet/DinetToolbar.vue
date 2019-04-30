597d8576a7b7d7c2ba07e12a99d7c12f8c14a066
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-04-30 14:27:55 -0500

    Dinet SPA 15

--- imports/ui/components/Dinet/DinetToolbar.vue
+++ imports/ui/components/Dinet/DinetToolbar.vue
@@ -1,16 +1,38 @@
 <template>
-  <v-toolbar dense>
+  <v-toolbar>
     <v-layout justify-space-between>
       <v-avatar color="grey lighten-4">
         <img src="img/Dinet_alt.png" alt="avatar">
       </v-avatar>
-      <v-icon>account_circle</v-icon>
+
+      <v-menu offset-y :nudge-bottom="true">
+        <v-btn icon slot="activator">
+          <v-icon>account_circle</v-icon>
+        </v-btn>
+
+        <v-list>
+          <v-list-tile v-for="(item,key) in profileMenu" :key="key" @click="item.action">
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
     </v-layout>
   </v-toolbar>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      profileMenu: [{ title: "Logout", action: () => this.logout }]
+    };
+  },
+  methods: {
+      logout(){
+          alert('Logout')
+      }
+  }
+};
 </script>
 
 <style>
