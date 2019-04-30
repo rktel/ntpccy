cdb184ee7f6f96ab230acf244abe8d6c3120240d
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-04-30 14:46:41 -0500

    Dinet SPA 20

--- imports/ui/components/Dinet/DinetToolbar.vue
+++ imports/ui/components/Dinet/DinetToolbar.vue
@@ -5,7 +5,7 @@
         <img src="img/Dinet_alt.png" alt="avatar">
       </v-avatar>
 
-      <v-menu>
+      <v-menu offset-x>
         <v-btn icon slot="activator">
           <v-icon>account_circle</v-icon>
         </v-btn>
