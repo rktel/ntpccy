a216659247345287b53644c8a277f949c9fdbe16
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-04-30 16:32:40 -0500

    Dinet SPA 39

--- imports/ui/components/Dinet/DinetContentToolbar.vue
+++ imports/ui/components/Dinet/DinetContentToolbar.vue
@@ -1,6 +1,6 @@
 <template>
-    <v-toolbar color="transparent">
-        <v-toolbar-title> <span class="title font-italic grey--text text--lighten-3">Control de Velocidad</span></v-toolbar-title>
+    <v-toolbar color="transparent" flat>
+        <v-toolbar-title> <span class="title font-italic grey--text text--lighten-1">Control de Velocidad</span></v-toolbar-title>
     </v-toolbar>
 </template>
 
