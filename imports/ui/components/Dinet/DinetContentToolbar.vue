72698155783ee574f2aa443cf6181cd26d6951e2
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-04-30 17:42:09 -0500

    Dinet SPA 42

--- imports/ui/components/Dinet/DinetContentToolbar.vue
+++ imports/ui/components/Dinet/DinetContentToolbar.vue
@@ -15,7 +15,7 @@
     >
       <v-text-field
         slot="activator"
-        label="Fecha de Actualizacion"
+        label="Dia"
         v-model="date"
         append-icon="event"
         placeholder=" "
@@ -23,9 +23,7 @@
         outline
       ></v-text-field>
       <v-date-picker v-model="date" scrollable>
-        <v-spacer></v-spacer>
-        <v-btn flat color="primary" @click="modal = false">Cancel</v-btn>
-        <v-btn flat color="primary" @click="$refs.dialog.save(date)">OK</v-btn>
+
       </v-date-picker>
     </v-dialog>
   </v-toolbar>
