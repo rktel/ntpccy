c7e80cf219e8de5a028d7be4188c724da87129cb
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-04-30 14:04:12 -0500

    Dinet SPA 13

--- imports/ui/components/Dinet/DinetToolbar.vue
+++ imports/ui/components/Dinet/DinetToolbar.vue
@@ -1,21 +1,17 @@
 <template>
-    <v-toolbar dense>
-                <v-avatar
-          :tile="tile"
-          :size="avatarSize"
-          color="grey lighten-4"
-        >
-          <img src="img/Dinet_alt.png" alt="avatar">
-        </v-avatar>
-    </v-toolbar>
+  <v-toolbar dense>
+    <v-layout justify-space-between>
+      <v-avatar color="grey lighten-4">
+        <img src="img/Dinet_alt.png" alt="avatar">
+      </v-avatar>
+      <v-icon>mdi-account_circle</v-icon>
+    </v-layout>
+  </v-toolbar>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
