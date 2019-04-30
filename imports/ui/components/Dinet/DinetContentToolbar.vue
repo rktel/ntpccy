42d2f1a18f8700c260c4cc16182807eddef643c4
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-04-30 17:59:18 -0500

    Dinet SPA 43

--- imports/ui/components/Dinet/DinetContentToolbar.vue
+++ imports/ui/components/Dinet/DinetContentToolbar.vue
@@ -22,10 +22,37 @@
         readonly
         outline
       ></v-text-field>
-      <v-date-picker v-model="date" scrollable>
-
+      <v-date-picker v-model="date" scrollable @input="modal = false">
+        <v-spacer></v-spacer>
+        <v-btn flat color="primary" @click="modal = false">Cancel</v-btn>
+        <v-btn flat color="primary" @click="$refs.dialog.save(date)">OK</v-btn>
       </v-date-picker>
     </v-dialog>
+<v-menu
+                      ref="menu"
+                      lazy
+                      :close-on-content-click="false"
+                      v-model="menu"
+                      transition="scale-transition"
+                      offset-y
+                      full-width
+                      :nudge-right="40"
+                      min-width="290px"
+                      :return-value.sync="date"
+                    >
+                      <v-text-field
+                        slot="activator"
+                        label="Picker in menu"
+                        v-model="date"
+                        prepend-icon="event"
+                        readonly
+                      ></v-text-field>
+                      <v-date-picker v-model="date" no-title scrollable>
+                        <v-spacer></v-spacer>
+                        <v-btn flat color="primary" @click="menu = false">Cancel</v-btn>
+                        <v-btn flat color="primary" @click="$refs.menu.save(date)">OK</v-btn>
+                      </v-date-picker>
+                    </v-menu>
   </v-toolbar>
 </template>
 
