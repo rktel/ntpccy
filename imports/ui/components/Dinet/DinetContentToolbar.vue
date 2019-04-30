66cbdd3a66822a7c419a9fea55b04f370fd7ea1f
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-04-30 18:03:42 -0500

    Dinet SPA 44

--- imports/ui/components/Dinet/DinetContentToolbar.vue
+++ imports/ui/components/Dinet/DinetContentToolbar.vue
@@ -4,55 +4,28 @@
       <span class="title font-italic grey--text">Control de Velocidad</span>
     </v-toolbar-title>
     <v-spacer></v-spacer>
-    <v-dialog
-      ref="dialog"
-      persistent
-      v-model="modal"
+
+    <v-menu
+      ref="menu"
       lazy
+      :close-on-content-click="false"
+      v-model="menu"
+      transition="scale-transition"
+      offset-y
       full-width
-      width="290px"
-      :return-value.sync="date"
-    >
+      :nudge-right="40"
+      min-width="290px"
+     >
       <v-text-field
         slot="activator"
-        label="Dia"
+        label="Picker in menu"
         v-model="date"
-        append-icon="event"
-        placeholder=" "
+        prepend-icon="event"
         readonly
-        outline
       ></v-text-field>
-      <v-date-picker v-model="date" scrollable @input="modal = false">
-        <v-spacer></v-spacer>
-        <v-btn flat color="primary" @click="modal = false">Cancel</v-btn>
-        <v-btn flat color="primary" @click="$refs.dialog.save(date)">OK</v-btn>
-      </v-date-picker>
-    </v-dialog>
-<v-menu
-                      ref="menu"
-                      lazy
-                      :close-on-content-click="false"
-                      v-model="menu"
-                      transition="scale-transition"
-                      offset-y
-                      full-width
-                      :nudge-right="40"
-                      min-width="290px"
-                      :return-value.sync="date"
-                    >
-                      <v-text-field
-                        slot="activator"
-                        label="Picker in menu"
-                        v-model="date"
-                        prepend-icon="event"
-                        readonly
-                      ></v-text-field>
-                      <v-date-picker v-model="date" no-title scrollable>
-                        <v-spacer></v-spacer>
-                        <v-btn flat color="primary" @click="menu = false">Cancel</v-btn>
-                        <v-btn flat color="primary" @click="$refs.menu.save(date)">OK</v-btn>
-                      </v-date-picker>
-                    </v-menu>
+      <v-date-picker v-model="date" no-title>
+       </v-date-picker>
+    </v-menu>
   </v-toolbar>
 </template>
 
