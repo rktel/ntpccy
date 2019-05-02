e02562552064333883bf49b3b1cdfd4e095982ef
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-05-02 15:58:42 -0500

    Dinet SPA Toolbar 36

---
+++ imports/ui/components/Dinet/DinetFooter.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-footer app>
+      <span class="px-3">&copy; {{ new Date().getFullYear() }}
+          
+      </span>
+    </v-footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
