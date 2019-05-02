7f3fb5b62171064855755546d5ce1980e3990d01
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-05-02 12:26:37 -0500

    Dinet SPA 97

--- imports/ui/components/Dinet/DinetContentToolbar.vue
+++ imports/ui/components/Dinet/DinetContentToolbar.vue
@@ -25,7 +25,7 @@
           <v-date-picker v-model="date" no-title @input="menu = false"></v-date-picker>
         </v-menu>
       </v-flex>
-      <v-flex v-else-if="dropdown === 'Mes'" xs12 lg2 color="blue">
+      <v-flex v-else-if="dropdown === 'Mes'" xs12 lg2 class="blue">
         <v-menu
           ref="menu2"
           :close-on-content-click="false"
@@ -43,7 +43,7 @@
           <v-date-picker v-model="date2" no-title @input="menu2 = false" type="month"></v-date-picker>
         </v-menu>
       </v-flex>
-      <v-flex xs12 lg2 color="red">
+      <v-flex xs12 lg2 class="red">
         <v-btn color="#11274c" dark>
           Buscar
         </v-btn>
