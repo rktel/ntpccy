268126a4726939620cffd63b159bbfae2851aede
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-05-02 15:02:23 -0500

    Dinet SPA Toolbar 29

--- imports/ui/components/Dinet/DinetContentChart.vue
+++ imports/ui/components/Dinet/DinetContentChart.vue
@@ -1,24 +1,33 @@
 <template>
- <section>
-     <v-layout row wrap>
-         <v-flex lg3 xs12>
-             <v-card>
-                 <v-btn>Mark</v-btn>
-             </v-card>
-         </v-flex>
-         <v-flex lg3 xs12>HI</v-flex>
-         <v-flex lg3 xs12>HI</v-flex>
-         <v-flex lg3 xs12>HI</v-flex>
-     </v-layout>
- </section>
+  <section>
+    <v-layout row wrap>
+      <v-flex lg3 xs12>
+        <v-card>
+          <v-btn>Mark</v-btn>
+        </v-card>
+      </v-flex>
+      <v-flex lg3 xs12>
+        <v-card>
+          <v-btn>Mark</v-btn>
+        </v-card>
+      </v-flex>
+      <v-flex lg3 xs12>
+        <v-card>
+          <v-btn>Mark</v-btn>
+        </v-card>
+      </v-flex>
+      <v-flex lg3 xs12>
+        <v-card>
+          <v-btn>Mark</v-btn>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </section>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
