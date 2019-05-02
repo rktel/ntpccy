69e33dff95a36ea1ddaa257b1c6902b232f77dc0
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-05-02 14:25:48 -0500

    Dinet SPA Toolbar 20

--- imports/ui/components/Dinet/DinetContentToolbar.vue
+++ imports/ui/components/Dinet/DinetContentToolbar.vue
@@ -43,7 +43,7 @@
           <v-date-picker v-model="date2" no-title @input="menu2 = false" type="month"></v-date-picker>
         </v-menu>
       </v-flex>
-      <v-flex xs12 lg1 class="red" pt-0>
+      <v-flex xs12 lg1 pt-0>
         <v-tooltip bottom>
           <v-btn fab slot="activator">
             <v-icon>search</v-icon>
