831335c4e688917569438c4b82d9f05fc5ba4a1d
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-05-02 15:32:10 -0500

    Dinet SPA Toolbar 34

--- imports/ui/components/Dinet/DinetContentChart.vue
+++ imports/ui/components/Dinet/DinetContentChart.vue
@@ -1,8 +1,8 @@
 <template>
   <section>
     <v-layout row wrap>
-      <v-flex lg3 xs12>
-        <v-card>
+      <v-flex lg3 xs12 h>
+        <v-card height="200">
           <v-btn>Mark</v-btn>
         </v-card>
       </v-flex>
@@ -22,16 +22,17 @@
         </v-card>
       </v-flex>
 
-      <v-flex lg6 xs12>
+      <v-flex lg9 xs12>
         <v-card>
           <v-btn>Jarvis</v-btn>
         </v-card>
       </v-flex>
-      <v-flex lg6 xs12>
+      <v-flex lg3 xs12>
         <v-card>
           <v-btn>Jarvis</v-btn>
         </v-card>
       </v-flex>
+
     </v-layout>
   </section>
 </template>
