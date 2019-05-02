3271f2d89eef508f9c7efa3e5ef394c1e0969fdc
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-05-02 15:08:56 -0500

    Dinet SPA Toolbar 32

--- imports/ui/components/Dinet/DinetToolbar.vue
+++ imports/ui/components/Dinet/DinetToolbar.vue
@@ -1,6 +1,6 @@
 <template>
   <v-toolbar app fixed>
-    <img v-if="!avatar" src="img/Dinet.png" alt="avatar" width="80">
+    <img v-if="!avatar" src="img/Dinet.png" alt="avatar" width="50">
     <img v-else src="img/Dinet_white.png" alt="avatar" width="80">
     <v-spacer></v-spacer>
     <v-speed-dial v-model="fab" direction="bottom" transition="slide-y-reverse-transition">
