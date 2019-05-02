1b3e4b147249066a1a90c6479cd375ca0f9e8338
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-05-02 16:07:25 -0500

    Dinet SPA Toolbar 40

--- imports/ui/components/Dinet/DinetFooter.vue
+++ imports/ui/components/Dinet/DinetFooter.vue
@@ -1,8 +1,8 @@
 <template>
     <v-footer app>
       <v-spacer></v-spacer>
-      <span class="px-3">&copy; {{ new Date().getFullYear() }}</span>
-      <img src="img/seclog.png" alt="Securitas" width="45">
+      <span class="px-1">&copy; {{ new Date().getFullYear() }}</span>
+      <img class="px-2" src="img/seclog.png" alt="Securitas" width="45">
     </v-footer>
 </template>
 
