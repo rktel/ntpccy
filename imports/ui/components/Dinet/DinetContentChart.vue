d213ebf1742f958caf42bcec4ea7edb4f301ee12
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-05-03 18:41:36 -0500

    Dinet SPA add vue-apexcharts 15

--- imports/ui/components/Dinet/DinetContentChart.vue
+++ imports/ui/components/Dinet/DinetContentChart.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
     <v-layout row wrap>
-      <v-flex lg3 xs12 h>
+      <v-flex lg3 xs12 class="grey lighten-2">
         <v-card height="200">
           <apexchart width="380" type="bar" :options="options1" :series="series1"></apexchart>
         </v-card>
