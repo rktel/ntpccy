ea922473c7375bffd463dff890a09bb1a402b262
rktel <rakinakuy@hotmail.com>
2019-05-09 17:05:33 -0500

    DinetContent 6

--- imports/ui/components/Dinet/DinetContent.vue
+++ imports/ui/components/Dinet/DinetContent.vue
@@ -1,6 +1,6 @@
 <template>
   <v-content>
-    <v-container text-xs-center grid-list-lg>
+    <v-container text-xs-center grid-list-xl>
       <v-layout align-center justify-center>
         <v-flex xs10>
           <!--DinetContentToolbar></DinetContentToolbar-->
