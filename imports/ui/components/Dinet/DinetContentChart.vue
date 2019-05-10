98bf2514503378ce75a53ac1a0fef229ddc0f045
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-05-10 14:44:35 -0500

    chart again 6

--- imports/ui/components/Dinet/DinetContentChart.vue
+++ imports/ui/components/Dinet/DinetContentChart.vue
@@ -45,16 +45,12 @@ export default {
     return {
       series: [
         {
-          name: "Net Profit",
-          data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
+          name: "#Eventos",
+          data: [76]
         },
         {
-          name: "Revenue",
-          data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
-        },
-        {
-          name: "Free Cash Flow",
-          data: [35, 41, 36, 26, 45, 48, 52, 53, 41]
+          name: "Distancia(Km)",
+          data: [35]
         }
       ],
       chartOptions: {
@@ -76,23 +72,10 @@ export default {
 
         xaxis: {
           categories: [
-            "Feb",
-            "Mar",
-            "Apr",
-            "May",
-            "Jun",
-            "Jul",
-            "Aug",
-            "Sep",
-            "Oct"
+            "1° Turno",
           ]
         },
-        yaxis: {
-          title: {
-            text: "$ (thousands)"
-          }
-        },
-        fill: {
+          fill: {
           opacity: 1
         },
         tooltip: {
