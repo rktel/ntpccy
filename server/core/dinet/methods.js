b03b8462282f5fc54d6a195275113f6caa818b80
rktel <rakinakuy@hotmail.com>
2019-05-15 13:25:01 -0500

    first last 4

--- server/core/dinet/methods.js
+++ server/core/dinet/methods.js
@@ -63,10 +63,10 @@ Meteor.methods({
             const exceso80 = report.exceso80;
             const primerEvento = report.primerEvento
             const ultimoEvento = report.ultimoEvento
-  //          const primerDistancia = primerEvento.counters.type[9]
-   //         const ultimoDistancia = ultimoEvento.counters.type[9]
+          const primerDistancia = primerEvento.counters
+          const ultimoDistancia = ultimoEvento.counters
 
-            console.log(primerEvento, ultimoEvento);
+            console.log(primerDistancia, ultimoDistancia);
             
         }
         return report
