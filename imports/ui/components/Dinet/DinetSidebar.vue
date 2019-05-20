3c4fa4dda884e258ff0cdaf4d624fdc46b347fe5
rktel <rakinakuy@hotmail.com>
2019-05-20 14:29:11 -0500

    Dinet text field 17

--- imports/ui/components/Dinet/DinetSidebar.vue
+++ imports/ui/components/Dinet/DinetSidebar.vue
@@ -3,26 +3,32 @@
     <v-layout column>
       <v-flex>
         <v-subheader>OBJETO</v-subheader>
-        <v-select
-          v-model="vehicle"
-          :items="plates"
-          placeholder="Vehiculo"
-          persistent-hint
-          height="20"
-          flat
-        ></v-select>
+        <v-divider></v-divider>
+        <div class="px-2">
+          <v-select
+            v-model="vehicle"
+            :items="plates"
+            placeholder="Vehiculo"
+            persistent-hint
+            height="20"
+            flat
+          ></v-select>
+        </div>
         <v-divider></v-divider>
       </v-flex>
       <v-flex>
         <v-subheader>PERIODO</v-subheader>
+        <v-divider></v-divider>
         <v-radio-group v-model="periodo" row>
           <v-radio label="Dia" value="dia"></v-radio>
           <v-radio label="Rango" value="rango"></v-radio>
           <v-radio label="Mes" value="mes"></v-radio>
         </v-radio-group>
+        <v-divider></v-divider>
       </v-flex>
       <v-flex>
         <v-subheader>ACCION</v-subheader>
+        <v-divider></v-divider>
         <v-btn small block>Buscar</v-btn>
       </v-flex>
     </v-layout>
@@ -40,10 +46,10 @@ export default {
       return Session.get("DNT_plates");
     }
   },
-  data(){
-      return{
-          periodo: "dia"
-      }
+  data() {
+    return {
+      periodo: "dia"
+    };
   }
 };
 </script>
