55dbf56265eae49c8332432d7cef89478b0d3d00
rktel <rakinakuy@hotmail.com>
2019-05-20 15:49:42 -0500

    Dinet text field 28

--- imports/ui/components/Dinet/DinetSidebar.vue
+++ imports/ui/components/Dinet/DinetSidebar.vue
@@ -20,11 +20,52 @@
         <v-subheader>PERIODO</v-subheader>
         <v-divider></v-divider>
         <div class="pa-4">
-          <v-radio-group v-model="periodo" row @change="changePeriodo">
-            <v-radio label="Dia" value="dia"></v-radio>
-            <v-radio label="Rango" value="rango"></v-radio>
-            <v-radio label="Mes" value="mes"></v-radio>
+          <v-radio-group v-model="period" row @change="changePeriod">
+            <v-radio label="Dia" value="day"></v-radio>
+            <v-radio label="Rango" value="range"></v-radio>
+            <v-radio label="Mes" value="month"></v-radio>
           </v-radio-group>
+          <v-menu
+            ref="pickerDay"
+            :close-on-content-click="false"
+            v-model="pickerDay"
+            transition="scale-transition"
+            v-if="period == 'day'"
+          >
+            <v-text-field
+              slot="activator"
+              v-model="pickerDayModel"
+              readonly
+              hint="Seleccione dia"
+              persistent-hint
+              height="20"
+              flat
+            ></v-text-field>
+            <v-date-picker v-model="pickerDayModel" no-title @input="pickerDay = false"></v-date-picker>
+          </v-menu>
+          <v-menu
+            ref="pickerMonth"
+            :close-on-content-click="false"
+            v-model="pickerMonth"
+            transition="scale-transition"
+            v-else-if="period == 'month'"
+          >
+            <v-text-field
+              slot="activator"
+              v-model="pickerMonthModel"
+              readonly
+              label="Seleccione mes"
+              persistent-hint
+              height="20"
+              flat
+            ></v-text-field>
+            <v-date-picker
+              v-model="pickerMonthModel"
+              no-title
+              @input="pickerMonth = false"
+              type="month"
+            ></v-date-picker>
+          </v-menu>
         </div>
         <v-divider></v-divider>
       </v-flex>
@@ -52,12 +93,16 @@ export default {
   },
   data() {
     return {
-      periodo: "dia"
+      period: "day",
+      pickerDayModel: new Date().toISOString().substr(0, 10),
+      pickerDay: false,
+      pickerMonthModel: null,
+      pickerMonth: false,
     };
   },
   methods: {
     changePeriodo() {
-      console.log(this.periodo);
+      console.log(this.period);
     }
   }
 };
