1520af7f252d4e4f74ebb1e3c0952f6dbd3568a2
rktel <rakinakuy@hotmail.com>
2019-05-20 23:46:01 -0500

    phantom 2

--- imports/ui/layouts/Phantom.vue
+++ imports/ui/layouts/Phantom.vue
@@ -1,13 +1,26 @@
 <template>
-    <h1>Phantom</h1>
+  <v-app>
+    <v-navigation-drawer app v-model="drawer"></v-navigation-drawer>
+    <v-toolbar color="primary" app>
+      <v-toolbar-side-icon @click="openDrawer"></v-toolbar-side-icon>
+    </v-toolbar>
+  </v-app>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      drawer: true
+    };
+  },
+  methods: {
+    openDrawer() {
+      this.drawer = !this.drawer;
+    }
+  }
+};
 </script>
 
 <style>
-
 </style>
