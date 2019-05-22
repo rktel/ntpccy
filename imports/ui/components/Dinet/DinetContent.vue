f6dd919672dd66c63f859aa55ce82b76dc6daaa5
rktel <rakinakuy@hotmail.com>
2019-05-22 13:19:11 -0500

    toolbar swipe 34

--- imports/ui/components/Dinet/DinetContent.vue
+++ imports/ui/components/Dinet/DinetContent.vue
@@ -2,7 +2,7 @@
   <v-content>
     <v-container text-xs-center grid-list-xl>
       <v-layout align-center justify-center>
-        <v-flex xs10>
+        <v-flex xs12>
           <DinetContentChart></DinetContentChart>
         </v-flex>
       </v-layout>
