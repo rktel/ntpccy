6d87d21dba31aebd6b4f67d7ce8bee19b02567d4
rktel <rakinakuy@hotmail.com>
2019-05-31 09:53:01 -0500

    Dinet colors 3

--- imports/ui/components/Dinet/DinetContentChart.vue
+++ imports/ui/components/Dinet/DinetContentChart.vue
@@ -140,7 +140,7 @@ export default {
             }
           },
           dataLabels: {
-            enabled: false
+            enabled: true
           },
           xaxis: {
             categories: seriesDays
