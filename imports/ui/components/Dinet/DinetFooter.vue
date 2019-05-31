c72b345d363019b7b2c6bde6eebea1a81800d889
rktel <rakinakuy@hotmail.com>
2019-05-31 16:04:00 -0500

    Dinet -.- 25

--- imports/ui/components/Dinet/DinetFooter.vue
+++ imports/ui/components/Dinet/DinetFooter.vue
@@ -1,9 +1,8 @@
 <template>
     <v-footer app>
-      <span> </span>
       <v-spacer></v-spacer>
       <span class="px-2">&copy; {{ new Date().getFullYear() }}</span>
-      <img class="mx-2" src="img/seclog.png" alt="Securitas" width="45">
+
     </v-footer>
 </template>
 
