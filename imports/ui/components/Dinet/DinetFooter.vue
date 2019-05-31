de903c361e274d2660b86202f35f9918fb9a2426
rktel <rakinakuy@hotmail.com>
2019-05-31 15:38:37 -0500

    Dinet -.- 18

--- imports/ui/components/Dinet/DinetFooter.vue
+++ imports/ui/components/Dinet/DinetFooter.vue
@@ -1,8 +1,11 @@
 <template>
-    <v-footer app absolute>
-      <v-spacer></v-spacer>
-      <span class="px-1">&copy; {{ new Date().getFullYear() }}</span>
-      <img class="mx-2" src="img/seclog.png" alt="Securitas" width="45">
+    <v-footer app>
+      <v-layout justify-center row wrap>
+          <v-flex py-3 xs12>
+            <span class="px-1">&copy; {{ new Date().getFullYear() }}</span>
+            <img class="mx-2" src="img/seclog.png" alt="Securitas" width="45">
+          </v-flex>
+      </v-layout>
     </v-footer>
 </template>
 
