7d3c457fdbfcb42b5f5b392039b95c1bf0158cd9
rktel <rakinakuy@hotmail.com>
2019-06-07 16:42:01 -0500

    Again Servosa 2

--- imports/ui/layouts/Servosa.vue
+++ imports/ui/layouts/Servosa.vue
@@ -1,13 +1,14 @@
 <template>
-    <h1>Servosa</h1>
+  <v-toolbar color="primary" dense>
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-toolbar-title>Servosa</v-toolbar-title>
+    <v-spacer></v-spacer>
+  </v-toolbar>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
