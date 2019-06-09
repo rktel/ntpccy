b08914621e39b71cb63dae841a3707cd1d159cbc
rktel <rakinakuy@hotmail.com>
2019-06-08 21:06:12 -0500

    Again Servosa 31

--- imports/ui/components/Servosa/Transfer.vue
+++ imports/ui/components/Servosa/Transfer.vue
@@ -1,23 +1,27 @@
 <template>
   <section>
-      <VLayout>
-          <VFlex xs5>
-              <VCard>
-                  <section>
-                      <VSubheader class="font-weight-medium">Opciones </VSubheader>
-                      <VSubheader>0/{{names.length}} elegidos </VSubheader>
-                  </section>
-                  <VDivider/>
-                  <VList dense>
-                      <VListTile v-for="(item) in names" :key="item">
-                          {{item}}
-                      </VListTile>
-                  </VList>
-              </VCard>
-          </VFlex>
-          <VFlex xs2></VFlex>
-          <VFlex xs5></VFlex>
-      </VLayout>
+    <VLayout>
+      <VFlex xs5>
+        <VCard>
+          <section>
+            <VList two-line>
+              <VListTile>
+                <v-list-tile-content>
+                  <v-list-tile-title>Opciones</v-list-tile-title>
+                  <v-list-tile-sub-title>0/{{names.length}}  elegidos</v-list-tile-sub-title>
+                </v-list-tile-content>
+              </VListTile>
+            </VList>
+          </section>
+          <VDivider/>
+          <VList dense>
+            <VListTile v-for="(item) in names" :key="item">{{item}}</VListTile>
+          </VList>
+        </VCard>
+      </VFlex>
+      <VFlex xs2></VFlex>
+      <VFlex xs5></VFlex>
+    </VLayout>
   </section>
 </template>
 
