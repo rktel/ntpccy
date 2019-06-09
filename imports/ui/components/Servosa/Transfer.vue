0e248a7ff9e04a853e5792dcd7edb08374aad90b
rktel <rakinakuy@hotmail.com>
2019-06-08 19:49:44 -0500

    Again Servosa 28

--- imports/ui/components/Servosa/Transfer.vue
+++ imports/ui/components/Servosa/Transfer.vue
@@ -1,13 +1,42 @@
 <template>
-    <h1>Transfer</h1>
+  <section>
+      <VLayout>
+          <VFlex xs5>
+              <VCard>
+                  <VCardTitle>
+                      <h3>Opciones</h3>
+                      <VSubheader>0/{{names.length}} elegidos </VSubheader>
+                  </VCardTitle>
+                  <VDivider/>
+                  <VList dense>
+                      <VListTile v-for="(item) in names" :key="item">
+                          {{item}}
+                      </VListTile>
+                  </VList>
+              </VCard>
+          </VFlex>
+          <VFlex xs2></VFlex>
+          <VFlex xs5></VFlex>
+      </VLayout>
+  </section>
 </template>
 
 <script>
+const NAMES = [
+  "Peppa",
+  "Gato con Botas",
+  "Mappets Baby",
+  "El Mundo de Luna",
+  "Kung FU Panda"
+];
 export default {
-
-}
+  data() {
+    return {
+      names: NAMES
+    };
+  }
+};
 </script>
 
 <style>
-
 </style>
