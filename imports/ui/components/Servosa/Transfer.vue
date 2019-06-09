25255d1ec66d9280ad00828c61f89d0e272fba24
rktel <rakinakuy@hotmail.com>
2019-06-09 00:12:38 -0500

    Again Servosa 69

--- imports/ui/components/Servosa/Transfer.vue
+++ imports/ui/components/Servosa/Transfer.vue
@@ -45,7 +45,34 @@
           
         </VLayout>
       </VFlex>
-      <VFlex xs5></VFlex>
+      <VFlex xs5>
+        <VCard>
+          <section>
+            <VList two-line dense>
+              <VListTile>
+                <v-list-tile-action>
+                  <v-checkbox></v-checkbox>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                  <v-list-tile-title>Elegidos</v-list-tile-title>
+                  <v-list-tile-sub-title>0/{{names.length}} elegidos</v-list-tile-sub-title>
+                </v-list-tile-content>
+              </VListTile>
+            </VList>
+          </section>
+          <VDivider/>
+          <VList dense :style="{height: 205+ 'px', overflowY: 'scroll'}">
+            <VListTile v-for="(item) in names" :key="item">
+              <v-list-tile-action>
+                <v-checkbox></v-checkbox>
+              </v-list-tile-action>
+              <VListTileContent>
+                <v-list-tile-sub-title>{{item}}</v-list-tile-sub-title>
+              </VListTileContent>
+            </VListTile>
+          </VList>
+        </VCard>
+      </VFlex>
     </VLayout>
   </section>
 </template>
