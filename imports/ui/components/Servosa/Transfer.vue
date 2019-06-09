ea47cab37079f4d33dd737ced2ea499575453f5c
rktel <rakinakuy@hotmail.com>
2019-06-08 21:59:54 -0500

    Again Servosa 33

--- imports/ui/components/Servosa/Transfer.vue
+++ imports/ui/components/Servosa/Transfer.vue
@@ -8,14 +8,21 @@
               <VListTile>
                 <v-list-tile-content>
                   <v-list-tile-title>Opciones</v-list-tile-title>
-                  <v-list-tile-sub-title>0/{{names.length}}  elegidos</v-list-tile-sub-title>
+                  <v-list-tile-sub-title>0/{{names.length}} elegidos</v-list-tile-sub-title>
                 </v-list-tile-content>
               </VListTile>
             </VList>
           </section>
           <VDivider/>
           <VList dense>
-            <VListTile v-for="(item) in names" :key="item">{{item}}</VListTile>
+            <VListTile v-for="(item) in names" :key="item">
+              <v-list-tile-action>
+                <v-checkbox></v-checkbox>
+              </v-list-tile-action>
+              <VListTileContent>
+                  <v-list-tile-sub-title>{{item}}</v-list-tile-sub-title>
+              </VListTileContent>
+            </VListTile>
           </VList>
         </VCard>
       </VFlex>
