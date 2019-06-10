7ee4f818aac4a688a20bbf1fd3afbeaa2c00da05
rktel <rakinakuy@hotmail.com>
2019-06-10 17:00:35 -0500

    Again Servosa 84

--- imports/ui/components/Servosa/Transfer.vue
+++ imports/ui/components/Servosa/Transfer.vue
@@ -1,33 +1,15 @@
 <template>
     <section>
-        <VLayout>
+        <VLayout justify-center column fill-height>
             <VFlex xs3>
                 <VCard>
-                    <!--section class="px-3">
-            <v-text-field label="Filtrar nombre" append-icon="search"></v-text-field>
-          </section>
-                    <VDivider/-->
-                    <section>
-                        <VList two-line dense>
-                            <VListTile>
-                                <v-list-tile-action>
-                                    <v-checkbox v-model="checkAll"></v-checkbox>
-                                </v-list-tile-action>
-                                <v-list-tile-content>
-                                    <v-list-tile-title>Opciones</v-list-tile-title>
-                                    <v-list-tile-sub-title>0/{{names.length}} elegidos</v-list-tile-sub-title>
-                                </v-list-tile-content>
-                            </VListTile>
-                        </VList>
-                    </section>
-                    <VDivider/>
                     <VList dense :style="{height: 205+ 'px', overflowY: 'scroll'}">
-                        <VListTile v-for="(item) in names" :key="item.name" @click>
+                        <VListTile v-for="value in left" :key="value">
                             <v-list-tile-action>
-                                <v-checkbox v-model="item.checked"></v-checkbox>
+                                <v-checkbox @change="checked.indexOf(value)!== -1"></v-checkbox>
                             </v-list-tile-action>
-                            <VListTileContent @click="item.checked=!item.checked">
-                                <v-list-tile-sub-title>{{item.name}}</v-list-tile-sub-title>
+                            <VListTileContent @click="handleToggle(value)">
+                                <v-list-tile-sub-title>{{item +1}}</v-list-tile-sub-title>
                             </VListTileContent>
                         </VListTile>
                     </VList>
@@ -35,42 +17,38 @@
             </VFlex>
             <VFlex xs1>
                 <VLayout align-center justify-center column fill-height>
-                    <VBtn color="primary" icon @click="transferRight">
+                    <VBtn
+                        color="primary"
+                        icon
+                        @click="handleCheckedRight"
+                        :disabled="leftChecked.length === 0"
+                    >
                         <VIcon>chevron_right</VIcon>
                     </VBtn>
-                    <VBtn color="primary" icon>
+                    <VBtn
+                        color="primary"
+                        icon
+                        @click="handleCheckedLeft"
+                        :disabled="rightChecked.length === 0"
+                    >
                         <VIcon>chevron_left</VIcon>
                     </VBtn>
                 </VLayout>
             </VFlex>
             <VFlex xs3>
                 <VLayout justify-center column fill-height>
-                    <VCard>
-                        <section>
-                            <VList two-line dense>
-                                <VListTile>
-                                    <v-list-tile-action>
-                                        <v-checkbox></v-checkbox>
-                                    </v-list-tile-action>
-                                    <v-list-tile-content>
-                                        <v-list-tile-title>Elegidos</v-list-tile-title>
-                                        <v-list-tile-sub-title>0/{{namesRight.length}} elegidos</v-list-tile-sub-title>
-                                    </v-list-tile-content>
-                                </VListTile>
-                            </VList>
-                        </section>
-                        <VDivider/>
-                        <VList dense :style="{height: 205+ 'px', overflowY: 'scroll'}">
-                            <VListTile v-for="(item) in namesRight" :key="item.name">
-                                <v-list-tile-action>
-                                    <v-checkbox></v-checkbox>
-                                </v-list-tile-action>
-                                <VListTileContent>
-                                    <v-list-tile-sub-title>{{item.name}}</v-list-tile-sub-title>
-                                </VListTileContent>
-                            </VListTile>
-                        </VList>
-                    </VCard>
+                <VCard>
+                    <VList dense :style="{height: 205+ 'px', overflowY: 'scroll'}">
+                        <VListTile v-for="value in right" :key="value">
+                            <v-list-tile-action>
+                                <v-checkbox @change="checked.indexOf(value)!== -1"></v-checkbox>
+                            </v-list-tile-action>
+                            <VListTileContent @click="handleToggle(value)">
+                                <v-list-tile-sub-title>{{item +1}}</v-list-tile-sub-title>
+                            </VListTileContent>
+                        </VListTile>
+                    </VList>
+                </VCard>
                 </VLayout>
             </VFlex>
         </VLayout>
@@ -78,42 +56,68 @@
 </template>
 
 <script>
-const NAMES = [
-    "Peppa",
-    "Gato con Botas",
-    "Mappets Baby",
-    "El Mundo de Luna",
-    "Kung Fu Panda",
-    "Monchichi",
-    "Mini Beat Power Rocket",
-    "Gaby and Alex"
-];
-let neoNames = [];
 export default {
     created() {
-        neoNames = NAMES.map((el, index) => {
-            return {
-                checked: false,
-                name: el
-            };
-        });
-        this.names = neoNames;
+        this.checked = [];
+        this.left = [0, 1, 2, 3];
+        this.right = [4, 5, 6, 7];
+        this.leftChecked = intersection(checked, left);
+        this.rightChecked = intersection(checked, right);
     },
     methods: {
-        transferRight() {
-            const namesRight = this.names.filter(el => el.checked);
-            console.log("namesRight:", namesRight);
-            this.namesRight = namesRight;
+        handleToggle(value) {
+            const currentIndex = this.checked.indexOf(value);
+            const newChecked = [...this.checked];
+
+            if (currentIndex === -1) {
+                newChecked.push(value);
+            } else {
+                newChecked.splice(currentIndex, 1);
+            }
+            this.checked = newChecked;
+        },
+
+        handleAllRight() {
+            this.right = this.right.concat(this.left);
+            this.left = [];
+        },
+
+        handleCheckedRight() {
+            this.right = this.right.concat(this.leftChecked);
+            this.left = not(this.left, this.leftChecked);
+            this.checked = not(this.checked, this.leftChecked);
+        },
+
+        handleCheckedLeft() {
+            this.left = this.left.concat(this.rightChecked);
+            this.right = not(this.right, this.rightChecked);
+            this.checked = not(this.checked, this.rightChecked);
+        },
+
+        handleAllLeft() {
+            this.left = this.left.concat(this.right);
+            this.right = [];
         }
     },
     data() {
         return {
-            names: [],
-            checkAll: false,
-            namesRight: []
+            checked: [],
+            left: [],
+            right: [],
+            leftChecked: [],
+            rightChecked: []
         };
     }
 };
+
+//Funciones de apoyo
+function not(a, b) {
+    return a.filter(value => b.indexOf(value) === -1);
+}
+
+function intersection(a, b) {
+    return a.filter(value => b.indexOf(value) !== -1);
+}
 </script>
 
 <style>
