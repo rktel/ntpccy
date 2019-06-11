9705883fd6001ba12b0ec3fe22babc1d74701db4
rktel <rakinakuy@hotmail.com>
2019-06-11 12:00:30 -0500

    Again Servosa c6

--- imports/ui/components/Servosa/Transfer.vue
+++ imports/ui/components/Servosa/Transfer.vue
@@ -5,40 +5,39 @@
 				<VList dense>
 					<section :style="{height: 250 + 'px', overflowY: 'scroll'}">
 						<VListTile v-for="item in left" :key="item.vehicle">
-                            <VListTileAction>
-                                <VCheckbox  v-model="item.checked" ></VCheckbox>
-                            </VListTileAction>
-                            <VListTileContent>
-                                {{item.vehicle}}
-                            </VListTileContent>
-                        </VListTile>
+							<VListTileAction>
+								<VCheckbox v-model="item.checked"></VCheckbox>
+							</VListTileAction>
+							<VListTileContent>{{item.vehicle}}</VListTileContent>
+						</VListTile>
 					</section>
 				</VList>
 			</VCard>
 		</VFlex>
 		<VFlex s1>
-            <VLayout column fill-height>
-                <VBtn color="success" icon @click="leftToRight">
-                    <VIcon>chevron_right</VIcon>
-                </VBtn>
-                <VBtn color="success" icon @click="rightToLeft">
-                    <VIcon>chevron_left</VIcon>
-                </VBtn>
-            </VLayout>
-        </VFlex>
+			<VLayout column fill-height align-center>
+				<VBtn color="success" icon @click="leftToRight">
+					<VIcon>chevron_right</VIcon>
+				</VBtn>
+				<VBtn color="success" icon @click="rightToLeft">
+					<VIcon>chevron_left</VIcon>
+				</VBtn>
+			</VLayout>
+		</VFlex>
 		<VFlex s3>
 			<VCard>
 				<VList dense>
 					<section :style="{height: 250 + 'px', overflowY: 'scroll'}">
 						<VListTile v-for="item in right" :key="item.vehicle">
-                            <VListTileContent>
-                                {{item.vehicle}}
-                            </VListTileContent>
-                        </VListTile>
+							<VListTileAction>
+								<VCheckbox v-model="item.checked"></VCheckbox>
+							</VListTileAction>
+							<VListTileContent>{{item.vehicle}}</VListTileContent>
+						</VListTile>
 					</section>
 				</VList>
-			</VCard>            
-        </VFlex>
+			</VCard>
+		</VFlex>
 	</VLayout>
 </template>
 
@@ -47,25 +46,29 @@ export default {
 	name: "Transfer",
 	props: ["universe"],
 	created() {
-        this.left = addChecked(this.universe);
+		this.left = addChecked(this.universe);
 		this.right = [];
 	},
 	methods: {
 		leftToRight() {
-            this.left = getCheckedFalse(this.left)
-            this.right = getCheckedTrue(this.left)
-        },
-        rightToLeft(){
-            this.right = getCheckedFalse(this.right)            
-            this.left = getCheckedTrue(this.right)
-        }
-    },
-    data(){
-        return{
-            left:[],
-            right:[]
-        }
-    }
+			const newLeft = getCheckedFalse(this.left);
+			const newRight = getCheckedTrue(this.left);
+			this.left = newLeft;
+			this.right = newRight;
+		},
+		rightToLeft() {
+			const newRight = getCheckedFalse(this.right);
+			const newLeft = getCheckedTrue(this.right);
+			this.left = newLeft;
+			this.right = newRight;
+		}
+	},
+	data() {
+		return {
+			left: [],
+			right: []
+		};
+	}
 };
 //End export default
 //Funciones de apoyo
