ef943731da8fe53fec16c45103eff8346f588890
rktel <rakinakuy@hotmail.com>
2019-06-11 18:47:09 -0500

    Again Servosa c33

--- imports/ui/layouts/Servosa.vue
+++ imports/ui/layouts/Servosa.vue
@@ -1,7 +1,7 @@
 <template>
 	<v-app>
 		<v-toolbar dense app clipped-left>
-			<v-toolbar-side-icon></v-toolbar-side-icon>
+			<v-toolbar-side-icon @click="drawer=!drawer"></v-toolbar-side-icon>
 			<img src="/img/Servosa.png" alt="Servosa" width="150">
 			<v-spacer></v-spacer>
 		</v-toolbar>
@@ -19,52 +19,14 @@
 					<v-subheader>PERIODO</v-subheader>
 					<v-divider></v-divider>
 					<div class="pa-3">
-						<v-radio-group v-model="period" row @change="changePeriod">
-							<v-radio label="Dia" value="day"></v-radio>
-							<v-radio label="Mes" value="month"></v-radio>
-							<v-radio label="Rango" value="range"></v-radio>
-						</v-radio-group>
-						<v-menu
-							ref="pickerDay"
-							:close-on-content-click="false"
-							v-model="pickerDay"
-							transition="scale-transition"
-							v-if="period == 'day'"
-						>
-							<v-text-field
-								slot="activator"
-								v-model="pickerDayModel"
-								readonly
-								height="20"
-								flat
-								label="Dia"
-							></v-text-field>
-							<v-date-picker v-model="pickerDayModel" no-title @input="pickerDay = false"></v-date-picker>
-						</v-menu>
-						<v-menu
-							ref="pickerMonth"
-							:close-on-content-click="false"
-							v-model="pickerMonth"
-							transition="scale-transition"
-							v-else-if="period == 'month'"
-						>
-							<v-text-field
-								slot="activator"
-								v-model="pickerMonthModel"
-								readonly
-								label="Mes"
-								height="20"
-								flat
-							></v-text-field>
-							<v-date-picker v-model="pickerMonthModel" no-title @input="pickerMonth = false" type="month"></v-date-picker>
-						</v-menu>
+
 						<section>
 							<v-menu
 								ref="pickerDayStart"
 								:close-on-content-click="false"
 								v-model="pickerDayStart"
 								transition="scale-transition"
-								v-if="period == 'range'"
+				
 							>
 								<v-text-field
 									slot="activator"
@@ -101,15 +63,13 @@
 					<v-subheader>ACCION</v-subheader>
 					<v-divider></v-divider>
 					<div class="pa-3">
-						<v-btn block color="primary">Buscar</v-btn>
+						<v-btn block color="primary" @click="onSRVSReport">Buscar</v-btn>
 					</div>
 				</v-flex>
 			</v-layout>
 		</v-navigation-drawer>
 		<VContent>
-			<VContainer>
-
-			</VContainer>
+			<VContainer></VContainer>
 		</VContent>
 	</v-app>
 </template>
@@ -132,23 +92,27 @@ export default {
 		onChildClick(data) {
 			console.log("data: ", data);
 			this.vehicles = data;
+		},
+		onSRVSReport() {
+			const T = "T";
+			const Z = ":00.000Z";
+
+			//const dateTimeStart = dateStart + T + timeStart + Z;
+			//const dateTimeEnd = dateEnd + T + timeEnd + Z;
+      if(this.vehicles.length>0){
+        console.log(this.vehicles, this.pickerDayStartModel, this.pickerDayEndModel);
+      }
 		}
 	},
 	data() {
 		return {
 			drawer: true,
-			pickerDayModel: new Date().toISOString().substr(0, 10),
-			pickerDay: false,
 			pickerDayStartModel: null,
 			pickerDayStart: false,
 			pickerDayEndModel: null,
 			pickerDayEnd: false,
-			pickerMonthModel: null,
-			pickerMonth: false,
-			loadingData: false,
 			plates: [],
 			vehicles: [],
-			period: "day"
 		};
 	}
 };
--- server/core/servosa/methods.js
+++ server/core/servosa/methods.js
@@ -1,8 +1,67 @@
 import { Servosa, ArrayPlates } from '../../../imports/api/collections'
 
-Meteor.methods({ 
+Meteor.methods({
     async SRVS_getPlates() {
-        const plates = ArrayPlates.find({name:"Servosa"}).fetch()
+        const plates = ArrayPlates.find({ name: "Servosa" }).fetch()
         return plates
     },
+    async  SRVS_getData(plates, dateTimeStart, dateTimeEnd) {
+        // 'FatigueAlarm', 'DistractionAlarm', 'No Rostro o No Conductor'
+        /**
+         *      ORIGINAL    NAME_EVENT
+         *      82          Distraccion
+         *      81          Fatiga
+         *      80          No Rostro
+         */
+        const eventOriginal_Array = [82, 81, 80]
+        // const arrayEvents = [81,82]
+
+        const report = await Servosa.rawCollection().
+            aggregate([
+                { $match: { 'events.vehicle': { $in: plates }, 'events.created': { $gte: dateTimeStart, $lte: dateTimeEnd } } },
+                { $unwind: '$events' },
+                { $match: { 'events.original': { $in: eventOriginal_Array } } },
+                {
+                    $group: {
+                        _id: '$events.vehicle',
+                        noRostro: {
+                            $sum: {
+                                $cond: [
+                                    { $eq: ['$events.original', 80] }, 1, 0
+                                ]
+                            }
+                        },
+                        fatiga: {
+                            $sum: {
+                                $cond: [
+                                    { $eq: ['$events.original', 81] }, 1, 0
+                                ]
+                            }
+                        },
+                        distraccion: {
+                            $sum: {
+                                $cond: [
+                                    { $eq: ['$events.original', 82] }, 1, 0
+                                ]
+                            }
+                        },
+                    },
+                },
+                { $project: { _id: 0, placa: '$_id', noRostro: '$noRostro', fatiga: '$fatiga', distraccion: '$distraccion' } },
+                { $sort: { 'placa': 1 } },
+
+            ]).toArray()
+
+
+        return report
+
+    }
 });
+
+/** Helpers Function */
+
+function addHours(datetime, hours) {
+    let date = new Date(datetime);
+    date.setHours(date.getHours() + hours);
+    return date.toISOString()
+}
