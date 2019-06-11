11f13f83fa4fa42703d6c9816321cb00de72b34b
rktel <rakinakuy@hotmail.com>
2019-06-11 11:08:28 -0500

    Again Servosa 100

--- imports/ui/components/Servosa/Transfer.vue
+++ imports/ui/components/Servosa/Transfer.vue
@@ -1,42 +1,50 @@
 <template>
-    <VLayout>
-        <ul>
-            <li v-for="item in right" :key="item.vehicle" @click="msg(item.vehicle, item.checked)">
-                {{item.vehicle}}
-            </li>
-        </ul>
-    </VLayout>
+	<VLayout fill-height>
+		<VFlex s3>
+			<VCard>
+				<VList>
+					<div :style="{height: 250 + 'px', overflowY: 'scroll'}">
+						<VListTile v-for="item in right" :key="item.vehicle">
+                            <VListTileContent>
+                                {{item.vehicle}}
+                            </VListTileContent>
+                        </VListTile>
+					</div>
+				</VList>
+			</VCard>
+		</VFlex>
+		<VFlex s1></VFlex>
+		<VFlex s3></VFlex>
+	</VLayout>
 </template>
 
 <script>
 export default {
-    name:'Transfer',
-    props:['universe'],
-    created(){
-      this.right = addChecked(this.universe)
-      this.left = []  
-    },
-    methods:{
-        msg(a,b){
-            alert(a +"," +b)
-        }
-    }
-}
+	name: "Transfer",
+	props: ["universe"],
+	created() {
+		this.right = addChecked(this.universe);
+		this.left = [];
+	},
+	methods: {
+		msg(a, b) {
+			alert(a + "," + b);
+		}
+	}
+};
 //End export default
 //Funciones de apoyo
 function addChecked(array) {
-    return array.map(el => ({vehicle: el, checked: false}) )
+	return array.map(el => ({ vehicle: el, checked: false }));
 }
-function getCheckedTrue(array){
-    return array.filter(el => el.checked===true)
+function getCheckedTrue(array) {
+	return array.filter(el => el.checked === true);
 }
-function getCheckedFalse(array){
-    return array.filter(el => el.checked===false)
+function getCheckedFalse(array) {
+	return array.filter(el => el.checked === false);
 }
-
 </script>
 
 <style>
-
 </style>
 
