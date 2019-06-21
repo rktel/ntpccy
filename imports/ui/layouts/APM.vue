7067cfe888a51e5bd2f9a3764ea3b5d962d4f6aa
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-06-21 17:02:12 -0500

    APM 7

--- imports/ui/layouts/APM.vue
+++ imports/ui/layouts/APM.vue
@@ -7,14 +7,9 @@
 						<v-card>
 							<v-card-title>Excel Report Convert</v-card-title>
 							<v-card-text>
-								<v-layout row justify-center>
-									<v-flex xs12 lg6>
-										<input type="file">
-									</v-flex>
-									<v-flex xs12 lg6>
-										<v-btn>Convertir</v-btn>
-									</v-flex>
-								</v-layout>
+								<input type="file">
+								<v-spacer></v-spacer>
+								<v-btn>Convertir</v-btn>
 							</v-card-text>
 						</v-card>
 					</v-flex>
