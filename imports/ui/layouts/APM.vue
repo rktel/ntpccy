60520d7cd8f5faaf6b7832ea997865980bab22fe
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-06-21 17:00:04 -0500

    APM 6

--- imports/ui/layouts/APM.vue
+++ imports/ui/layouts/APM.vue
@@ -1,29 +1,32 @@
 <template>
-    <v-app>
-        <v-content>
-            <v-container fill-height>
-                <v-layout row justify-center>
-                    <v-flex xs12 lg8>
-                        <v-card>
-                            <v-card-title>Excel Report Convert</v-card-title>
-                            <v-card-text>
-                                <input type="file"/>
-                                <v-btn>Convertir</v-btn>
-                            </v-card-text>
-                        </v-card>
-                    </v-flex>
-                </v-layout>
-            </v-container>
-        </v-content>
-    </v-app>
+	<v-app>
+		<v-content>
+			<v-container fill-height>
+				<v-layout row justify-center>
+					<v-flex xs12 lg8>
+						<v-card>
+							<v-card-title>Excel Report Convert</v-card-title>
+							<v-card-text>
+								<v-layout row justify-center>
+									<v-flex xs12 lg6>
+										<input type="file">
+									</v-flex>
+									<v-flex xs12 lg6>
+										<v-btn>Convertir</v-btn>
+									</v-flex>
+								</v-layout>
+							</v-card-text>
+						</v-card>
+					</v-flex>
+				</v-layout>
+			</v-container>
+		</v-content>
+	</v-app>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
