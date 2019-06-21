d1f6bf29eded173cc41d8d981e14f8ae64e3a844
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-06-21 17:13:37 -0500

    APM 11

--- imports/ui/layouts/APM.vue
+++ imports/ui/layouts/APM.vue
@@ -10,7 +10,7 @@
 								<v-toolbar flat>
 									<input type="file">
 									<v-spacer></v-spacer>
-									<v-btn icon fab>
+									<v-btn fab small dark color="grey">
                                         <v-icon>transform</v-icon>
                                     </v-btn>
 								</v-toolbar>
