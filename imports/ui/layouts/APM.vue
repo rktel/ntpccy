99cba7a501763338976e12fee3a22bfb6c15920d
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-06-21 16:34:08 -0500

    APM 2

--- imports/ui/layouts/APM.vue
+++ imports/ui/layouts/APM.vue
@@ -1,5 +1,20 @@
 <template>
-    <h1>APM</h1>
+    <v-app>
+        <v-content>
+            <v-container fill-height>
+                <v-layout row justify-center>
+                    <v-flex xs8>
+                        <v-card>
+                            <v-card-title>Excel Convert</v-card-title>
+                            <v-card-text>
+                                <v-text-field type="file"></v-text-field>
+                            </v-card-text>
+                        </v-card>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
