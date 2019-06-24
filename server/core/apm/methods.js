119fc3b06ad7b3dfd23304f1bc86ec993f944df5
rktel <rakinakuy@hotmail.com>
2019-06-24 13:51:45 -0500

    FileReade 4

--- server/core/apm/methods.js
+++ server/core/apm/methods.js
@@ -1,9 +1,9 @@
 Meteor.methods({
-    async  APM_uploadS: (bstr, name) => {
+    async  APM_uploadS(bstr, name) {
         const wb = await XLSX.read(bstr, { type: 'binary' });
         return wb
     },
-    async  APM_uploadU: (ab, name) => {
+    async  APM_uploadU(ab, name) {
         const wb = await XLSX.read(ab, { type: 'array' });
         return wb
     },
