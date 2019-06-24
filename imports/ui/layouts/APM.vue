d3828a8ba20f7431238e80c78f47546bd3276c96
rktel <rakinakuy@hotmail.com>
2019-06-24 17:26:40 -0500

    FileReade 32

--- imports/ui/layouts/APM.vue
+++ imports/ui/layouts/APM.vue
@@ -73,7 +73,25 @@ export default {
 			//	else reader.readAsArrayBuffer(file);
 		},
 		transformFile() {
-			alert("hello");
+			/* original data */
+var data = [
+    {"name":"John", "city": "Seattle"},
+    {"name":"Mike", "city": "Los Angeles"},
+    {"name":"Zach", "city": "New York"}
+];
+
+/* this line is only needed if you are not adding a script tag reference */
+// if(typeof XLSX == 'undefined') XLSX = require('xlsx');
+
+/* make the worksheet */
+var ws = XLSX.utils.json_to_sheet(data);
+
+/* add to workbook */
+var wb = XLSX.utils.book_new();
+XLSX.utils.book_append_sheet(wb, ws, "People");
+
+/* write workbook */
+XLSX.write(wb, {bookType:'xlsx', type:'buffer'});
 		}
 	}
 };
