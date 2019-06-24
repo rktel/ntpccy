a3c5c8eb289d91a9878054681a386680823cbef8
rktel <rakinakuy@hotmail.com>
2019-06-24 14:28:09 -0500

    FileReade 13

--- server/core/apm/methods.js
+++ server/core/apm/methods.js
@@ -3,7 +3,11 @@ import XLSX from "xlsx";
 Meteor.methods({
     APM_uploadS(bstr, name) {
         const wb = XLSX.read(bstr, { type: 'binary' });
-        const json = XLSX.utils.sheet_to_json(wb)
+        /* DO SOMETHING WITH workbook HERE */
+        var first_sheet_name = wb.SheetNames[0];
+        /* Get worksheet */
+        var ws = wb.Sheets[first_sheet_name];
+        const json = XLSX.utils.sheet_to_json(ws)
         console.log(json)
         return wb
     },
