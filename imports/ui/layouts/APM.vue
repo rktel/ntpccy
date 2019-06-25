8884933fc312a5c475de2729af0b1c1d78cb1325
rktel <rakinakuy@hotmail.com>
2019-06-25 12:35:42 -0500

    FileReade 68

--- imports/ui/layouts/APM.vue
+++ imports/ui/layouts/APM.vue
@@ -43,16 +43,24 @@ export default {
 					/* load the first worksheet */
 
 					if (!err) {
+						/*
 						let out = [];
 
 						const ws = wb.Sheets[wb.SheetNames[0]];
-						const json = XLSX.utils.sheet_to_json(ws,{ header: 1 },{ raw: false });
+						const json = XLSX.utils.sheet_to_json(
+							ws,
+							{ header: 1 },
+							{ raw: false }
+						);
 						console.log(json);
 
 						json.forEach((el, index) => {
 							if (index > 1) {
 								out.push({
-									Tipo: el[0].substr(	el[0].indexOf("Current Speed") + 15, 7),
+									Tipo: el[0].substr(
+										el[0].indexOf("Current Speed") + 15,
+										7
+									),
 									Dispositivo: el[1],
 									Persona: el[2],
 									Localizacion: el[3],
@@ -60,12 +68,15 @@ export default {
 								});
 							}
 						});
-
+*/
+/*
 						json2excel({
 							data: out,
 							name: "Reporte",
-							//formateDate: "yyyy/mm/dd"
+							formateDate: "yyyy/mm/dd"
 						});
+*/
+
 					}
 
 					/* generate HTML table and enable export 
@@ -83,40 +94,37 @@ export default {
 		}
 	}
 };
-
+/*
 function ExcelDateToJSDate(serial) {
-    var utc_days  = Math.floor(serial - 25569);
-    var utc_value = utc_days * 86400;                                        
-    var date_info = new Date(utc_value * 1000);
- 
-    var fractional_day = serial - Math.floor(serial) + 0.0000001;
- 
-    var total_seconds = Math.floor(86400 * fractional_day);
- 
-    var seconds = total_seconds % 60;
- 
-    total_seconds -= seconds;
- 
-    var hours = Math.floor(total_seconds / (60 * 60));
-    var minutes = Math.floor(total_seconds / 60) % 60;
- 
-	let isoString = new Date(date_info.getFullYear(), date_info.getMonth(), date_info.getDate(), hours, minutes, seconds);
+	var utc_days = Math.floor(serial - 25569);
+	var utc_value = utc_days * 86400;
+	var date_info = new Date(utc_value * 1000);
+
+	var fractional_day = serial - Math.floor(serial) + 0.0000001;
+
+	var total_seconds = Math.floor(86400 * fractional_day);
+
+	var seconds = total_seconds % 60;
+
+	total_seconds -= seconds;
+
+	var hours = Math.floor(total_seconds / (60 * 60));
+	var minutes = Math.floor(total_seconds / 60) % 60;
+
+	let isoString = new Date(date_info.getFullYear(),date_info.getMonth(),date_info.getDate(),hours,minutes,seconds	);
 	// isoString = isoString.setHours(isoString.getHours() + 19);
-	isoString.setHours( isoString.getHours() + 19 );
-	isoString =  isoString.toISOString()
-	isoString = isoString.split("T")
-	let date = isoString[0].split("-")
-	let time = isoString[1].split(".")
+	isoString.setHours(isoString.getHours() + 19);
+	isoString = isoString.toISOString();
+	isoString = isoString.split("T");
+	let date = isoString[0].split("-");
+	let time = isoString[1].split(".");
 
-	date = date[2]+"/"+date[1]+"/"+date[0]
-	time = time[0]
+	date = date[2] + "/" + date[1] + "/" + date[0];
+	time = time[0];
 
-	return date + " " + time
-
-
-
-
- }
+	return date + " " + time;
+}
+*/
 </script>
 
 <style>
--- server/core/apm/methods.js
+++ server/core/apm/methods.js
@@ -2,7 +2,33 @@ import XLSX from "xlsx";
 
 Meteor.methods({
     APM_uploadS(bstr, name) {
-       return XLSX.read(bstr, { type: 'binary', cellDates: true});
+       let wb =  XLSX.read(bstr, { type: 'binary', cellDates: true});
+       let out = [];
+
+       const ws = wb.Sheets[wb.SheetNames[0]];
+       const json = XLSX.utils.sheet_to_json(
+           ws,
+           { header: 1 },
+           { raw: false }
+       );
+     //  console.log(json);
+
+       json.forEach((el, index) => {
+           if (index > 1) {
+               out.push({
+                   Tipo: el[0].substr(
+                       el[0].indexOf("Current Speed") + 15,
+                       7
+                   ),
+                   Dispositivo: el[1],
+                   Persona: el[2],
+                   Localizacion: el[3],
+                   Fecha: ExcelDateToJSDate(el[7])
+               });
+           }
+       });
+       console.log(out);
+
     },
     APM_uploadU(ab, name) {
         return XLSX.read(ab, { type: 'array' });
@@ -10,23 +36,34 @@ Meteor.methods({
 });
 
 function ExcelDateToJSDate(serial) {
-    var utc_days  = Math.floor(serial - 25569);
-    var utc_value = utc_days * 86400;                                        
-    var date_info = new Date(utc_value * 1000);
- 
-    var fractional_day = serial - Math.floor(serial) + 0.0000001;
- 
-    var total_seconds = Math.floor(86400 * fractional_day);
- 
-    var seconds = total_seconds % 60;
- 
-    total_seconds -= seconds;
- 
-    var hours = Math.floor(total_seconds / (60 * 60));
-    var minutes = Math.floor(total_seconds / 60) % 60;
- 
-    return new Date(date_info.getFullYear(), date_info.getMonth(), date_info.getDate(), hours, minutes, seconds);
- }
+	var utc_days = Math.floor(serial - 25569);
+	var utc_value = utc_days * 86400;
+	var date_info = new Date(utc_value * 1000);
+
+	var fractional_day = serial - Math.floor(serial) + 0.0000001;
+
+	var total_seconds = Math.floor(86400 * fractional_day);
+
+	var seconds = total_seconds % 60;
+
+	total_seconds -= seconds;
+
+	var hours = Math.floor(total_seconds / (60 * 60));
+	var minutes = Math.floor(total_seconds / 60) % 60;
+
+	let isoString = new Date(date_info.getFullYear(),date_info.getMonth(),date_info.getDate(),hours,minutes,seconds	);
+	// isoString = isoString.setHours(isoString.getHours() + 19);
+	isoString.setHours(isoString.getHours() + 19);
+	isoString = isoString.toISOString();
+	isoString = isoString.split("T");
+	let date = isoString[0].split("-");
+	let time = isoString[1].split(".");
+
+	date = date[2] + "/" + date[1] + "/" + date[0];
+	time = time[0];
+
+	return date + " " + time;
+}
 /*
 
         let out = [];
